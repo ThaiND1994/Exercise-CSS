a13f8af58cf4f602802549346edf95b9bcadc275
NguyenNgocThai <thaind1994@gmail.com>
2019-11-09 22:35:04 +0700

    Fix Slider

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $('.sliderone').slick({});
+    $('.sliderone')
+    .not(".slick-initialized")
+    .slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        dots:true,
+        prevArrow:".site-slider .slider-btn .prev",
+        nextArrow:".site-slider .slider-btn .next",
+    });
   });
